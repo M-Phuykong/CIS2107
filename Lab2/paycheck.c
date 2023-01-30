6a0176a9dee038835f6a8f71960e260eec452427
Phuykong-OfficePC <m.phuykong@gmail.com>
2023-01-30 13:53:07 -0500

    started paycheck

--- Lab2/paycheck.c
+++ Lab2/paycheck.c
@@ -1,3 +1,31 @@
 // Phuykong Meng, 2023-01-30, CIS2107
 // Lab 02 Paycheck
-// 
+//
+// Write a C program called paycheck to calculate the paycheck for a Temple 
+// employee based on the hourlySalary, weeklyTime (working for maximum 40 
+// hours) and overtime (working for more than 40 hours). 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+
+    int employee_number;
+    int hourly_salary;
+    int weekly_time;
+
+    printf("Welcome to \"TEMPLE HUMAN RESOURCES\"\n");
+
+    printf("Enter Employee Number: ");
+    scanf("%d", &employee_number);
+
+    printf("Enter Hourly Salary: ");
+    scanf("%d", &hourly_salary);
+
+    printf("Enter Weekly Time: ");
+    scanf("%d", &weekly_time);
+
+    printf("%d, %d, %d\n", employee_number, hourly_salary, weekly_time);
+
+
+}
--- Lab2/revenue.c
+++ Lab2/revenue.c
@@ -1,3 +1,9 @@
 // Phuykong Meng, 2023-01-30, CIS2107
 // Lab 02 Revenue
-// 
+//
+// Write a C program called revenue to calculate the revenue from a sale based on the unit price and quantity of a product input by 
+// the user.
+
+
+
+
